91a95f1f33d212af2cfc36e0deeda325e2d4bef2
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-05-21 22:44:32 +0200

    Modifica menù post

--- Traveller/components/Travel-Componets/textcomponent.js
+++ Traveller/components/Travel-Componets/textcomponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TouchableWithoutFeedback, Modal, StyleSheet } from 'react-native';
 import { ComponentStyles } from './componentStyle';
 import { Badge } from '@react-native-material/core';
 import { getData } from '../../shared/data/localdata';
@@ -20,37 +20,16 @@ export default function TextComponent({ item, home, travel, loadPosts }) {
     }, [])
 
     return (
-        <TouchableWithoutFeedback
-            onPress={() => {
-                setShowMenu(false);
-            }}
-        >
-            <View style={ComponentStyles.card}>
-                {
-                    ((item.creator == userData.username) && !home) ?
-                        <View
-                            style={{ position: "absolute", top: 0, right: 0, zIndex: 99 }}
-                        >
-                            <TouchableOpacity
-                                style={{ position: "absolute", top: 5, right: 5, zIndex: 100 }}
-                                onPress={() => {
-                                    showMenu ? setShowMenu(false) : setShowMenu(true);
-                                }}>
-                                <Image source={require("../../assets/image/icona-more-cerchio.png")} style={{ width: 20, height: 20, tintColor: "lightgray" }} />
-                            </TouchableOpacity>
-                        </View>
-                        : null
-
-                }
-
-                {
-                    (showMenu) ?
-                        <View style={{ position: "absolute", top: 30, right: 10, zIndex: 100, backgroundColor: "white", borderRadius: 10, padding: 10, borderWidth: 1, borderColor: "lightgray" }}>
+        <View style={ComponentStyles.card}>
+            <Modal transparent visible={showMenu} animationType='slide' >
+                <TouchableWithoutFeedback onPress={() => setShowMenu(false)}>
+                    <View style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.2)" }}>
+                        <View style={style.content}>
                             <TouchableOpacity
                                 onPress={() => {
                                     item.pinned = !item.pinned;
 
-                                    axios.post(serverLink + "api/post/updatePinPost", { param : item })
+                                    axios.post(serverLink + "api/post/updatePinPost", { param: item })
                                         .then((response) => {
                                             setShowMenu(false);
                                             loadPosts(item.travel)
@@ -60,20 +39,20 @@ export default function TextComponent({ item, home, travel, loadPosts }) {
                                         })
                                 }}>
                                 <View
-                                    style={{ borderBottomWidth: 1, borderBottomColor: "lightgray", paddingBottom: 5, flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}
+                                    style={{ borderBottomWidth: 1, borderBottomColor: "lightgray", paddingBottom: 10, flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}
                                 >
-                                    <Image source={require("../../assets/image/pin.png")} style={{ width: 15, height: 15, tintColor: "lightgray", marginRight: 5 }} />
+                                    <Image source={require("../../assets/image/pin.png")} style={{ width: 22, height: 22, tintColor: "lightgray", marginRight: 10 }} />
                                     {
                                         (item.pinned) ?
-                                            <Text style={{ fontFamily: font.montserrat, fontSize: 15 }}>Unpin</Text>
+                                            <Text style={{ fontFamily: font.montserrat, fontSize: 20 }}>Rimuovi pin</Text>
                                             :
-                                            <Text style={{ fontFamily: font.montserrat, fontSize: 15 }}>Pin</Text>
+                                            <Text style={{ fontFamily: font.montserrat, fontSize: 20 }}>Aggiungi pin</Text>
                                     }
                                 </View>
                             </TouchableOpacity>
                             <TouchableOpacity
                                 onPress={() => {
-                                    axios.post(serverLink + "api/post/deletePost", { id : item._id })
+                                    axios.post(serverLink + "api/post/deletePost", { id: item._id })
                                         .then((response) => {
                                             setShowMenu(false);
                                             loadPosts(item.travel)
@@ -84,38 +63,65 @@ export default function TextComponent({ item, home, travel, loadPosts }) {
                                 }}>
 
                                 <View
-                                    style={{ paddingTop: 5, flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}
+                                    style={{ paddingTop: 10, flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}
                                 >
-                                    <Image source={require("../../assets/image/icona-cestino.png")} style={{ width: 15, height: 15, tintColor: "red", marginRight: 5 }} />
-                                    <Text style={{ fontFamily: font.montserrat, fontSize: 15, color: "red" }}>Elimina</Text>
+                                    <Image source={require("../../assets/image/icona-cestino.png")} style={{ width: 22, height: 22, tintColor: "red", marginRight: 10 }} />
+                                    <Text style={{ fontFamily: font.montserrat, fontSize: 20, color: "red" }}>Elimina post</Text>
                                 </View>
                             </TouchableOpacity>
                         </View>
-                        : null
-                }
-
-
-                {(item.pinned && !home) ?
-                    <View style={ComponentStyles.pinned}>
-                        <Image source={require("../../assets/image/pin.png")} style={{ width: 20, height: 20, marginRight: 5, tintColor: "lightgray" }} />
-                        <Text style={ComponentStyles.pinnedText}>Fissato in alto</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+            {
+                ((item.creator == userData.username) && !home) ?
+                    <View
+                        style={{ position: "absolute", top: 0, right: 0, zIndex: 99 }}
+                    >
+                        <TouchableOpacity
+                            style={{ position: "absolute", top: 5, right: 5, zIndex: 100 }}
+                            onPress={() => {
+                                showMenu ? setShowMenu(false) : setShowMenu(true);
+                            }}>
+                            <Image source={require("../../assets/image/icona-more-cerchio.png")} style={{ width: 20, height: 20, tintColor: "lightgray" }} />
+                        </TouchableOpacity>
                     </View>
                     : null
-                }
 
-                {(home) ?
-                    <Badge label={travel} style={ComponentStyles.Badge} labelStyle={ComponentStyles.BadgeText} />
-                    : null
-                }
+            }
 
-                <View style={ComponentStyles.headerContainer}>
-                    <View style={ComponentStyles.nameContainer}>
-                        <Text style={ComponentStyles.nameText}>@{item.creator}</Text>
-                    </View>
-                    <Text style={ComponentStyles.datetimeText} >{item.dateTime}</Text>
+            {(item.pinned && !home) ?
+                <View style={ComponentStyles.pinned}>
+                    <Image source={require("../../assets/image/pin.png")} style={{ width: 20, height: 20, marginRight: 5, tintColor: "lightgray" }} />
+                    <Text style={ComponentStyles.pinnedText}>Fissato in alto</Text>
                 </View>
-                <Text style={ComponentStyles.contentText} >{item.content}</Text>
+                : null
+            }
+
+            {(home) ?
+                <Badge label={travel} style={ComponentStyles.Badge} labelStyle={ComponentStyles.BadgeText} />
+                : null
+            }
+
+            <View style={ComponentStyles.headerContainer}>
+                <View style={ComponentStyles.nameContainer}>
+                    <Text style={ComponentStyles.nameText}>@{item.creator}</Text>
+                </View>
+                <Text style={ComponentStyles.datetimeText} >{item.dateTime}</Text>
             </View>
-        </TouchableWithoutFeedback>
+            <Text style={ComponentStyles.contentText} >{item.content}</Text>
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    content: {
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: "white",
+        width: "100%",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 20,
+    }
+})
